ac8c69bf338a8bcffaaaace04684f4b7bec1c949
Pritanshu Kumar <pritanshukumar434@gmail.com>
2026-08-06 19:09:37 +0530

    Update real_file_zipper_gui.py

--- real_file_zipper_gui.py
+++ real_file_zipper_gui.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox
 import zipfile 
-import os # Day 1 more activation      
+import os # Day 1 more activation        
  
 #Compression Code  
 def compress():
